faac4a791b7ce68202048a6af5a3bb7e1bc0a622
Maria Fischinger <mmfischinger@gmail.com>
2013-09-10 18:31:46 -0500

    .

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
-
-  // PSEUDO-CODE:
+    // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
+  $('form').on('submit', function(event){
   //   2- prevent the default action for that event from happening
+    event.preventDefault();
   //   3- generate a random number between 1 and 6 using JavaScript
+    var randomnumber=Math.floor(Math.random()*5 + 1);
   //   4- use jQuery to submit an AJAX post to the form's action
-  //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
+    $.post("/rolls",{value: randomnumber}, function(result){
+  //   5- when the AJAX post is done, replace the contents of the "#die" DIV 
+  //      in the DOM using jQuery
+       // console.log(result);
+       // alert(result);
+       //Why doesn't the below item work?
+      // $("#die").html(result);
+      $("body").html(result);
+
+    });
+  });
 });
